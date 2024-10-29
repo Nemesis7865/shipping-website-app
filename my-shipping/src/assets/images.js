57c21284aa706f397e99c64ffcc417f6cff221ef
Nemesis7865 <emmanuelaganmwonyi581@gmail.com>
2024-10-29 08:41:51 +0100

    Shipping

--- my-shipping/src/assets/images.js
+++ my-shipping/src/assets/images.js
@@ -2,4 +2,5 @@ export {default as logo} from "./logo.png";
 export {default as img1} from "./image.jpg";
 export {default as img2} from "./ima.jpg";
 export {default as img3} from "./img.jpg";
-export {default as hero} from "./hero.jpg"
+export {default as hero} from "./hero.jpg";
+export {default as backgroundImg} from "./fixed.jpg"
--- my-shipping/src/layout/Pages.jsx
+++ my-shipping/src/layout/Pages.jsx
@@ -57,7 +57,8 @@ const ImageSlider = () => {
 
 
   return (
-    <div className="relative w-full max-w-8xl">
+    <div className="relative w-full max-w-8xl ">
+      {/* Image div */}
       <div className={`relative ${imageHeight} w-full  rounded-lg overflow-hidden  h-[400px] sm:h-[500px] md:h-[600px] lg:h-[700px] xl:h-[800px]`}>
         <img
           src={images[currentIndex].src}
